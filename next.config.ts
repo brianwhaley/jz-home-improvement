6db6f1e40a91fb05087b7a788cfe28e467bd3541
Brian Whaley <brian.whaley@gmail.com>
2026-01-16 14:43:41 -0500

    chore: initial commit from pixelated-template

---
+++ next.config.ts
@@ -0,0 +1,48 @@
+ 
+import type { NextConfig } from "next";
+import path from 'path';
+
+const nextConfig: NextConfig = {
+
+	reactStrictMode: true,
+
+	experimental: {
+    	optimizeCss: true,
+  	},
+	transpilePackages: ['@pixelated-tech/components'],
+	trailingSlash: false,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	env: {
+		PIXELATED_CONFIG_JSON: process.env.PIXELATED_CONFIG_JSON,
+		PIXELATED_CONFIG_B64: process.env.PIXELATED_CONFIG_B64,
+	},
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+				port: "",
+				pathname: "**",
+			},
+		],
+	},
+
+	async redirects() {
+		return [];
+	},
+
+	turbopack: {},
+
+	webpack: (config: any) => {
+		if (!config.resolve) config.resolve = {};
+		if (!config.resolve.alias) config.resolve.alias = {};
+		config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+		return config;
+	},
+	
+};
+
+export default nextConfig;
---
+++ src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,15 @@
+"use client" 
+
+import React from 'react'
+import { PageSection } from '@pixelated-tech/components';
+
+export default function Home() {
+  return (
+    <>
+		<PageSection columns={1} maxWidth="1024px" id="header-section">
+			<h1>Jz Home Improvement</h1>
+			<p>Welcome to Jz Home Improvement</p>
+		</PageSection>
+    </>
+  )
+}
